4bf8540b20a6696a3309b6a595beb2b5a51deb78
Pop Plays <85472155+popQA17@users.noreply.github.com>
2022-01-29 18:38:43 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Router, Link } from 'react-static'
 //
 import Routes from 'react-static-routes'
 
+import { Transition, animated } from 'react-spring'
+
 import './app.css'
 
 export default () => (
@@ -14,7 +16,29 @@ export default () => (
         <Link to="/blog">Blog</Link>
       </nav>
       <div className="content">
-        <Routes />
+              <Routes>
+        {({ routePath, getComponentForPath }) => {
+          // Using the routePath as the key, both routes will render at the same time for the transition
+          return (
+            <Transition
+              native
+              items={routePath}
+              from={{ transform: 'translateY(100px)', opacity: 0 }}
+              enter={{ transform: 'translateY(0px)', opacity: 1 }}
+              leave={{ transform: 'translateY(100px)', opacity: 0 }}
+            >
+              {item => props => {
+                const Comp = getComponentForPath(item)
+                return (
+                  <animated.div style={props}>
+                    <Comp />
+                  </animated.div>
+                )
+              }}
+            </Transition>
+          )
+        }}
+      </Routes>
       </div>
     </div>
   </Router>
